beeff7ee12b95d75c29430c7f4174fb225b30efa
ricksondpenha <ricksondpenha@gmail.com>
2020-12-07 23:44:57 +0530

    added badge to cart icon

--- lib/pages/shoppingcart/cartBadge.dart
+++ lib/pages/shoppingcart/cartBadge.dart
@@ -1,12 +1,24 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import '../pages.dart';
 
 class CartBadge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      splashRadius: 25,
-      icon: Icon(Icons.shopping_cart_rounded),
-      onPressed: () => Navigator.of(context).pushNamed("/cart"),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+      child: Badge(
+        badgeContent: Text(
+          "2",
+          style: context.accentTextTheme.bodyText1,
+        ),
+        alignment: Alignment.center,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(100),
+          child: Icon(Icons.shopping_cart_rounded),
+          onTap: () => Navigator.of(context).pushNamed("/cart"),
+        ),
+      ),
     );
   }
 }
